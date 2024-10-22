ad516bcfda615c5c48aa9a5af3491dde368b78ac
priscillamelo <priscillamelo73@gmail.com>
2024-10-21 22:10:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_brick_breaker
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/ufrn/eaj/tads/flutter_brick_breaker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ufrn.eaj.tads.flutter_brick_breaker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
