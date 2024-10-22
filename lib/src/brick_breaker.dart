55e2b444393b513e9a2db24c229100fb3614d7ae
priscillamelo <priscillamelo73@gmail.com>
2024-10-22 08:47:31 -0300

    feat: add 'ball' component in the world

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flutter_brick_breaker/src/components/components.dart';
 import 'package:flutter_brick_breaker/src/components/play_area.dart';
 import 'package:flutter_brick_breaker/src/config.dart';
 
@@ -16,6 +18,7 @@ class BrickBreaker extends FlameGame {
 
   double get width => size.x;
   double get height => size.y;
+  final random = math.Random();
 
   @override
   FutureOr<void> onLoad() async {
@@ -23,5 +26,16 @@ class BrickBreaker extends FlameGame {
 
     camera.viewfinder.anchor = Anchor.topLeft;
     world.add(PlayArea());
+
+    world.add(
+      Ball(
+          velocity: Vector2((random.nextDouble() - 0.5) * width, height * 0.2)
+              .normalized()
+            ..scale(height / 4),
+          position: size / 2,
+          radius: ballRadius),
+    );
+
+    debugMode = true;
   }
 }
